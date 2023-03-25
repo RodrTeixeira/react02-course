5049f8f8e51cb68b663e5f26084a884ac17b4a75
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-03-25 08:41:30 -0300

    :zap: general update 05

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Construção de site",
+          type: "deposit",
+          category: "Outros",
+          amount: 3000,
+          createdAt: new Date("2023-03-23 22:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Outros Novamente",
+          amount: 2000,
+          createdAt: new Date("2023-03-14 11:00:00"),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = "api"
 
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "transaction 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createAt: new Date()
-        }
-      ]
+      return this.schema.all("transaction");
+    })
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create("transaction", data);
     })
   }
 })
