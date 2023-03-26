3c1575cef555c9cf4f8ecc9ab72d49f81f89ee7d
Rodrigo Teixeira <rodr.g.teixeira@gmail.com>
2023-03-26 08:19:25 -0300

    :zap: files update

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -4,12 +4,14 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionsContext, TransactionsProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement("#root");
 
 export function App() {
 
   const [ isNewTransactionModalOpen, setIsNewTransactionModalOpen  ] = useState(false);
+  
 
   function handleOpenNewTransactionModal() {
       setIsNewTransactionModalOpen(true);
@@ -20,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
 
@@ -31,7 +33,7 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
